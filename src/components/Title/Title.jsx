8359b47772d6e6c4d4cb3ae927ad4792e05c8434
haena7908 <yhn7908@gmail.com>
2024-12-29 16:02:51 +0900

    Feat: add Title component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const Title = () => {
+  return(
+    <header className='title'>
+      <h1>Image Search</h1>
+      <p>By <a href='https://unsplash.com/ko'>Unsplash</a></p>
+    </header>
+  );
+}
+
+export default Title;
