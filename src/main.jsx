99d24bdea938382ea3c1af62e9e92face021ae86
haena7908 <yhn7908@gmail.com>
2024-12-29 16:06:15 +0900

    Refactor: delete unnecessary message

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-//const response = await fetch(`https://api.unsplash.com/search/photos?query=${searchTerm}&${defaultParams}&per_page=50`);
-// await fech(`https://api.unsplash.com/search/photos?query=${word}&${defaultParams}&per_page=50`);
