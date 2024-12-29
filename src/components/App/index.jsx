30826894c93da85384289e3ff87fa1b953dda97a
haena7908 <yhn7908@gmail.com>
2024-12-29 15:45:51 +0900

    Feat: add App components

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Title from '../Tiltle/Title';
+import Images from '../images';
+
+export default function App() {
+  return (
+    <div>
+      <Title />
+      <Images />
+    </div>
+  );
+}
