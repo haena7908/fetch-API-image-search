6647d7fcb13b2175ebb897f2f3fcedc638092bc4
haena7908 <yhn7908@gmail.com>
2024-12-29 20:15:38 +0900

    Feat: add utils

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+async function responseApi() {
+  try{
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${word}&${defaultParams}&per_page=50`);
+
+    if(!response.ok) {
+      throw new Error("fetch error");
+    }
+
+    const data = await response.json();
+
+    return data.results;
+
+  }catch(error) {
+    console.log("error", error);
+  }
+}
